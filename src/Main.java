7a02753de16e7798356824eae834094a4b4f649a
souvikpal2000 <spal62588@gmail.com>
2022-10-15 09:48:59 +0530

    2nd Commit

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,128 @@ public class Main {
 		InputStreamReader isr=new InputStreamReader(System.in);
 		BufferedReader br=new BufferedReader(isr);
 		
-		System.out.print("Enter UserID : ");
-		String userId = br.readLine();
-		System.out.println("");
-		
 		UserDAO dao = new UserDAO();
 		
-		String userType = dao.getUserType(userId);
-		System.out.println("User Type of " + userId + " : " + userType);
+		System.out.println("Display UserType");
+		System.out.println("****************");
+		System.out.println("");
 		
-		String message01 = dao.getIncorrectAttempts(userId);
-		System.out.println("Incorrect Attempts of " + userId + " : " + message01);
+		System.out.print("Enter UserID present in DB : ");
+		String userId01 = br.readLine();
+		String userType = dao.getUserType(userId01);
+		System.out.println("User Type of " + userId01 + " : " + userType);
 		
-		String message02 = dao.changeUserType(userId);
-		System.out.println("UserType of " + userId + " : " + message02);
+		System.out.println("");
+		
+		System.out.println("Display Incorrect Attempts");
+		System.out.println("**************************");
+		System.out.println("");
+		
+		System.out.print("Enter UserID present in DB : ");
+		String userId02 = br.readLine();
+		String message01 = dao.getIncorrectAttempts(userId02);
+		System.out.println("Incorrect Attempts of " + userId02 + " : " + message01);
+		
+		System.out.println("");
+		
+		System.out.println("Change UserType to Admin");
+		System.out.println("************************");
+		System.out.println("");
+		
+		System.out.print("Enter UserID present in DB : ");
+		String userId03 = br.readLine();
+		String message02 = dao.changeUserType(userId03);
+		System.out.println("Changed UserType of " + userId03 + " : " + message02);
 		
 		System.out.println("");
 		
 		int count = dao.getLockStatus();
 		System.out.println("Total Number of Lock Status equal to ZERO : " + count);
 		
+		System.out.println("");
+		
+		System.out.println("Change Name");
+		System.out.println("***********");
+		System.out.println("");
+		
+		System.out.print("Enter UserID present in DB : ");
+		String userId04 = br.readLine();
+		System.out.print("Enter Any Name to Update Name of UserID " + userId04 + " : ");
+		String name = br.readLine();
+		String message03 = dao.changeName(userId04, name);
+		System.out.println("Name of UserID " + userId04 + " Updated : " + message03);
+		
+		System.out.println("");
+		
+		System.out.println("Change Admin Password");
+		System.out.println("*********************");
+		System.out.println("");
+		
+		System.out.print("Enter New Password for Admins : ");
+		String password = br.readLine();
+		String message04 = dao.changePassword(password);
+		System.out.println("Admin Password Updated : " + message04);
+		
+		System.out.println("");
+		
+		System.out.println("Add New User");
+		System.out.println("************");
+		System.out.println("");
+		
+		System.out.print("Enter UserID                     : ");
+		String newUserId = br.readLine();
+		System.out.print("Enter Password                   : ");
+		String newPassword = br.readLine();
+		System.out.print("Enter Name                       : ");
+		String newName = br.readLine();
+		System.out.print("Enter Incorrect Attempts         : ");
+		int newIncorrectAttempts = Integer.parseInt(br.readLine());
+		System.out.print("Enter Lock Status                : ");
+		int newLockStatus = Integer.parseInt(br.readLine());
+		String newUserType = "";
+		while(true) {
+			System.out.print("Enter User Type (Admin/Employee) : ");
+			newUserType = br.readLine();
+			if(newUserType.equalsIgnoreCase("Admin") || newUserType.equalsIgnoreCase("Employee")) {
+				break;
+			}
+		}
+		
+		System.out.println("");
+		
+		UserBean newUser = new UserBean(newUserId, newPassword, newName, newIncorrectAttempts, newLockStatus, newUserType);
+		String message05 = dao.addUser_1(newUser);
+		System.out.println("New User Added : " + message05);
+		
+		System.out.println("");
+		
+		System.out.println("Add New User with Lock Status 0");
+		System.out.println("*******************************");
+		System.out.println("");
+		
+		System.out.print("Enter UserID                     : ");
+		String newUserId02 = br.readLine();
+		System.out.print("Enter Password                   : ");
+		String newPassword02 = br.readLine();
+		System.out.print("Enter Name                       : ");
+		String newName02 = br.readLine();
+		System.out.print("Enter Incorrect Attempts         : ");
+		int newIncorrectAttempts02 = Integer.parseInt(br.readLine());
+		System.out.print("Enter Lock Status                : ");
+		int newLockStatus02 = Integer.parseInt(br.readLine());
+		String newUserType02 = "";
+		while(true) {
+			System.out.print("Enter User Type (Admin/Employee) : ");
+			newUserType02 = br.readLine();
+			if(newUserType02.equalsIgnoreCase("Admin") || newUserType02.equalsIgnoreCase("Employee")) {
+				break;
+			}
+		}
+		
+		System.out.println("");
+		
+		UserBean newUser02 = new UserBean(newUserId02, newPassword02, newName02, newIncorrectAttempts02, newLockStatus02, newUserType02);
+		String message06 = dao.addUser_2(newUser02);
+		System.out.println("New User Added : " + message06);
 	}
 }
--- src/UserDAO.java
+++ src/UserDAO.java
@@ -95,4 +95,113 @@ class UserDAO{
 		}
 		return count;
 	}
+	
+	public String changeName(String id, String name) {
+		DBConnection connec = new DBConnection();
+		Connection con;
+		String message = null;
+		try {
+			con = connec.getConnection();
+			String query = "UPDATE usertb SET Name=? WHERE UserID=?";
+			PreparedStatement pst = con.prepareStatement(query);
+			pst.setString(1, name);
+			pst.setString(2, id);
+			int row = pst.executeUpdate();
+			if(row == 1) {
+				message = "Success";
+			}else {
+				message = "Failed";
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return message;
+	}
+	
+	public String changePassword(String password) {
+		DBConnection connec = new DBConnection();
+		Connection con;
+		String message = null;
+		try {
+			con = connec.getConnection();
+			String query = "UPDATE usertb SET Password=? WHERE UserType=?";
+			PreparedStatement pst = con.prepareStatement(query);
+			pst.setString(1, password);
+			pst.setString(2, "Admin");
+			int row = pst.executeUpdate();
+			if(row > 0) {
+				message = "Success";
+			}else {
+				message = "Failed";
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return message;
+	}
+	
+	public String addUser_1(UserBean bean) {
+		DBConnection connec = new DBConnection();
+		Connection con;
+		String message = null;
+		try {
+			con = connec.getConnection();
+			String query = "INSERT INTO usertb VALUES(?,?,?,?,?,?)";
+			PreparedStatement pst = con.prepareStatement(query);
+			pst.setString(1, bean.getUserId());
+			pst.setString(2, bean.getPassword());
+			pst.setString(3, bean.getName());
+			pst.setInt(4, bean.getIncorrectAttempts());
+			pst.setInt(5, bean.getLockStatus());
+			pst.setString(6, bean.getUserType());
+			int row = pst.executeUpdate();
+			if(row > 0) {
+				message = "Success";
+			}else {
+				message = "Fail";
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			message = "Fail";
+		}
+		return message;
+	}
+	
+	public String addUser_2(UserBean bean) {
+		DBConnection connec = new DBConnection();
+		Connection con;
+		String message = null;
+		try {
+			if(bean.getLockStatus() != 0) {
+				message = "Fail";
+			} 
+			else {
+				con = connec.getConnection();
+				String query = "INSERT INTO usertb VALUES(?,?,?,?,?,?)";
+				PreparedStatement pst = con.prepareStatement(query);
+				pst.setString(1, bean.getUserId());
+				pst.setString(2, bean.getPassword());
+				pst.setString(3, bean.getName());
+				pst.setInt(4, bean.getIncorrectAttempts());
+				pst.setInt(5, bean.getLockStatus());
+				pst.setString(6, bean.getUserType());
+				int row = pst.executeUpdate();
+				if(row > 0) {
+					message = "Success";
+				}else {
+					message = "Fail";
+				}
+			}	
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			message = "Fail";
+		}
+		return message;
+	}
 }
